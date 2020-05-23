c92af7ca6f4a3f2da4297e3234f978492fd82687
Nikita Tupikov <nikita.tupikov@developex.net>
2020-05-23 13:49:59 +0300

    Removed some profane words

--- message_helpers/words_source.py
+++ message_helpers/words_source.py
@@ -1,23 +1,19 @@
 profane_words = [
-    "dolboy'eb", "ebalnik", "ebalo", "ebalom sch'elkat", "gol", "mudack", "opizdenet", "osto'eblo", "ostokhuitel'no",
-    "ot'ebis", "otmudohat", "otpizdit", "otsosi", "padlo", "pedik", "perdet", "petuh", "pidar gnoinyj", "pizda",
+    "dolboy'eb", "ebalnik", "ebalo", "ebalom sch'elkat", "mudak", "eblo", "ostokhuitel'no",
+    "ot'ebis", "otpizdit", "otsosi", "padlo", "pedik", "pidar", "pizda",
     "pizdato", "pizdatyi", "piz'det", "pizdetc", "pizdoi nakryt'sja", "pizd'uk", "piz`dyulina", "podi ku'evo",
-    "poeben", "po'imat' na konchik", "po'iti posrat", "po khuy", "poluchit pizdy", "pososi moyu konfetku", "prissat",
+    "poeben", "po'imat' na konchik", "po khuy", "poluchit pizdy", "pososi moyu konfetku", "prissat",
     "proebat", "promudobl'adsksya pizdopro'ebina", "propezdoloch", "prosrat", "raspeezdeyi", "raspizdatyi", "raz'yebuy",
-    "raz'yoba", "s'ebat'sya", "shalava", "styervo", "sukin syn", "svodit posrat", "svoloch", "trakhat'sya", "pidor",
-    "pidoras", "trimandoblydskiy pizdoproyob", "ubl'yudok", "uboy", "u'ebitsche", "vafl'a", "vafli lovit", "v pizdu",
-    "vyperdysh", "vzdrochennyi", "yeb vas", "za'ebat", "zaebis", "zalupa", "zalupat", "zasranetc", "zassat",
-    "zlo'ebuchy",
-    "бздёнок", "блядки", "блядовать", "блядство", "блядь", "бугор", "во пизду", "встать раком", "выёбываться", "гандон",
-    "говно", "говнюк", "голый", "дать пизды", "дерьмо", "дрочить", "другой дразнится", "ёбарь", "ебать", "ебать-копать",
-    "ебло", "ебнуть", "ёб твою мать", "жопа", "жополиз", "играть на кожаной флейте", "измудохать",
-    "какая разница", "как два пальца обоссать", "курите мою трубку", "лысого в кулаке гонять", "малофья", "манда",
-    "мандавошка", "мент", "муда", "мудило", "мудозвон", "наебать", "наебениться", "наебнуться", "на фиг", "на хуй",
-    "на хую вертеть", "на хуя", "нахуячиться", "невъебенный", "не ебет", "ни за хуй собачу", "ни хуя",
-    "обоссаться можно", "один ебётся", "опесдол", "офигеть", "охуеть", "охуительно", "половое сношение", "пидор",
-    "пидорас", "сиськи", "спиздить", "срать", "ссать", "траxать", "ты мне ваньку не валяй", "фига", "хапать",
+    "raz'yoba", "s'ebat'sya", "shalava", "styervo", "sukin syn", "pidor",
+    "pidoras", "trimandoblydskiy pizdoproyob", "ubl'yudok", "u'ebitsche", "v pizdu",
+    "vzdrochennyi", "za'ebat", "zaebis", "zalupa", "zalupat", "zasranetc", "zassat", "zlo'ebuchy",
+    "блядки", "блядовать", "блядство", "блядь", "бугор", "во пизду", "встать раком", "выёбываться", "гандон",
+    "говно", "говнюк", "пизды", "ебать", "ебать-копать", "ебло", "ебнуть", "ёб твою мать", "играть на кожаной флейте",
+    "обоссать", "малофья", "манда", "мандавошка", "мудило", "мудозвон", "наебать", "наебениться", "наебнуться",
+    "на хуй", "на хую вертеть", "на хуя", "нахуячиться", "невъебенный", "не ебет", "ни за хуй собачу", "ни хуя",
+    "обоссаться можно", "охуеть", "охуительно", "пидор", "пидорас", "спиздить", "траxать",
     "хер с ней", "хер с ним", "хохол", "хрен", "хуёво", "хуёвый", "хуем груши околачивать", "хуеплет", "хуило",
-    "хуйнёй страдать", "хуиня", "хуй", "хуйнуть", "хуй пинать"
+    "хуйнёй страдать", "хуйня", "хуй", "хуйнуть", "хуй пинать"
 ]
 
 cheat_words = [
