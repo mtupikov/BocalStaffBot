14d313ca6c05b802255595a2b98177ca890823fb
Nikita Tupikov <nikita.tupikov@developex.net>
2020-05-25 23:41:49 +0300

    Ratelimited bot output

--- message_handlers/handlers.py
+++ message_handlers/handlers.py
@@ -1,3 +1,5 @@
+import random
+
 from message_helpers.helper import *
 from discord import message
 from discord.ext import commands
@@ -17,34 +19,38 @@ async def on_message_impl(msg: message, bot: commands.bot):
         await _handle_suggestion(msg)
         return
 
+    random_condition = random.random() > 0.7
+
     text = msg.content.lower()
     profane_condition = check_profane_condition(text)
-    cheat_condition = check_cheat_condition(text)
-    to_bot_condition = check_ask_bot_condition(text, bot.user.id)
-    formatted_message_id = format_to_user_address(msg.author.id)
-    djigurda_condition = check_djigurda_condition(text)
-    boys_condition = check_boys_condition(text)
-    valera_condition = check_valera_condition(text)
-    ogo_condition = check_ogo_condition(text)
-    boyan_condition = check_boyan_condition(text)
-    temperature_condition = check_temperature_condition(text)  # TODO temp handling
-    p2p_condition = check_p2p_condition(text)
 
     if profane_condition:
         await msg.add_reaction('❗')  # exclamation mark
-    elif cheat_condition:
-        await msg.channel.send('CHEATING IS SLAVERY!')
-    elif to_bot_condition:
-        await msg.channel.send(f'{formatted_message_id}, ask peer on left, and then on right.')
-    elif djigurda_condition:
-        await msg.channel.send('http://coub.com/view/l7dseqm')
-    elif boys_condition:
-        await msg.channel.send('http://coub.com/view/zaf54')
-    elif valera_condition:
-        await msg.channel.send('https://coub.com/view/zmte7')
-    elif ogo_condition:
-        await msg.channel.send('https://coub.com/view/zq15r')
-    elif boyan_condition:
-        await msg.channel.send('https://coub.com/view/1ceayt1i')
-    elif p2p_condition:
-        await msg.channel.send('https://www.youtube.com/watch?v=XGG_4BLssrg')
+    elif random_condition:
+        cheat_condition = check_cheat_condition(text)
+        to_bot_condition = check_ask_bot_condition(text, bot.user.id)
+        formatted_message_id = format_to_user_address(msg.author.id)
+        djigurda_condition = check_djigurda_condition(text)
+        boys_condition = check_boys_condition(text)
+        valera_condition = check_valera_condition(text)
+        ogo_condition = check_ogo_condition(text)
+        boyan_condition = check_boyan_condition(text)
+        temperature_condition = check_temperature_condition(text)  # TODO temp handling
+        p2p_condition = check_p2p_condition(text)
+
+        if cheat_condition:
+            await msg.channel.send('CHEATING IS SLAVERY!')
+        elif to_bot_condition:
+            await msg.channel.send(f'{formatted_message_id}, ask peer on left, and then on right.')
+        elif djigurda_condition:
+            await msg.channel.send('http://coub.com/view/l7dseqm')
+        elif boys_condition:
+            await msg.channel.send('http://coub.com/view/zaf54')
+        elif valera_condition:
+            await msg.channel.send('https://coub.com/view/zmte7')
+        elif ogo_condition:
+            await msg.channel.send('https://coub.com/view/zq15r')
+        elif boyan_condition:
+            await msg.channel.send('https://coub.com/view/1ceayt1i')
+        elif p2p_condition:
+            await msg.channel.send('https://www.youtube.com/watch?v=XGG_4BLssrg')
