3e46f77711e441c13426f9606961b501f8bf34b5
Nikita Tupikov <nikita.tupikov@developex.net>
2020-05-25 23:36:25 +0300

    Words must be without spaces

--- message_helpers/words_source.py
+++ message_helpers/words_source.py
@@ -1,23 +1,23 @@
 profane_words = [
-    "dolboy'eb", "ebalnik", "ebalo", "ebalom sch'elkat", "mudak", "eblo", "ostokhuitel'no",
+    "dolboy'eb", "ebalnik", "ebalo", "ebalom", "mudak", "eblo", "ostokhuitel'no",
     "ot'ebis", "otpizdit", "otsosi", "padlo", "pedik", "pidar", "pizda",
     "pizdato", "pizdatyi", "piz'det", "pizdetc", "pizdoi nakryt'sja", "pizd'uk", "piz`dyulina", "podi ku'evo",
-    "poeben", "po'imat' na konchik", "po khuy", "poluchit pizdy", "pososi moyu konfetku", "prissat",
-    "proebat", "promudobl'adsksya pizdopro'ebina", "propezdoloch", "prosrat", "raspeezdeyi", "raspizdatyi", "raz'yebuy",
+    "poeben", "po'imat' khuyn'a", "khuy", "pizdy", "prissat",
+    "proebat", "pizdopro'ebina", "propezdoloch", "prosrat", "raspeezdeyi", "raspizdatyi", "raz'yebuy",
     "raz'yoba", "s'ebat'sya", "shalava", "styervo", "sukin syn", "pidor",
-    "pidoras", "trimandoblydskiy pizdoproyob", "ubl'yudok", "u'ebitsche", "v pizdu",
+    "pidoras", "pizdoproyob", "ubl'yudok", "u'ebitsche", "v pizdu",
     "vzdrochennyi", "za'ebat", "zaebis", "zalupa", "zalupat", "zasranetc", "zassat", "zlo'ebuchy",
     "блядки", "блядовать", "блядство", "блядь", "бугор", "во пизду", "встать раком", "выёбываться", "гандон",
-    "говно", "говнюк", "пизды", "ебать", "ебать-копать", "ебло", "ебнуть", "ёб твою мать", "играть на кожаной флейте",
+    "говно", "говнюк", "пизды", "ебать", "ебать-копать", "ебло", "ебнуть", "ёбнуть",
     "обоссать", "малофья", "манда", "мандавошка", "мудило", "мудозвон", "наебать", "наебениться", "наебнуться",
-    "на хуй", "на хую вертеть", "на хуя", "нахуячиться", "невъебенный", "не ебет", "ни за хуй собачу", "ни хуя",
-    "обоссаться можно", "охуеть", "охуительно", "пидор", "пидорас", "спиздить", "траxать",
-    "хер с ней", "хер с ним", "хохол", "хрен", "хуёво", "хуёвый", "хуем груши околачивать", "хуеплет", "хуило",
-    "хуйнёй страдать", "хуйня", "хуй", "хуйнуть", "хуй пинать"
+    "хую", "хуя", "нахуячиться", "невъебенный", "не ебет", "ни за хуй собачу", "ни хуя",
+    "охуеть", "охуительно", "пидор", "пидорас", "спиздить", "траxать",
+    "хер", "хохол", "хрен", "хуёво", "хуёвый", "хуем", "хуеплет", "хуило",
+    "хуйнёй", "хуйня", "хуй", "хуйнуть", "хуй пинать"
 ]
 
 cheat_words = [
-    "чітер", "чітєр", "чітінг", "читинг", "читер", "читера", "чітера", "cheat", "cheating", "-42", "- 42", "чіт", "чит",
+    "чітер", "чітєр", "чітінг", "читинг", "читер", "читера", "чітера", "cheat", "cheating", "-42", "чіт", "чит",
     "читерить", "читерили", "чітерити", "чітерили", "чітаки", "читаки", "читерят", "чітерять"
 ]
 
